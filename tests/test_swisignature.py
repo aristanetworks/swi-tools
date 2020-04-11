9a90ff965781938c28ddb01b24f27e724c39c099
mbwang <mbwang@berkeley.edu>
2020-04-10 18:34:01 -0700

    switools: more test files

---
+++ tests/test_swisignature.py
@@ -0,0 +1,147 @@
+# Copyright (c) 2018 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+
+import unittest
+import hashlib
+import os
+import shutil
+import tempfile
+import zipfile
+
+from switools import swisignature
+from switools import verifyswi
+
+import MockSigningServer
+
+SIG_FILE_NAME = 'swi-signature'
+
+class TestSwiSignature( unittest.TestCase ):
+
+    def setUp( self ):
+        self.test_dir = tempfile.mkdtemp()
+        self.test_swi = self._makeTestSwi( 'EOS.swi' )
+        self.root_crt = self._writeFile( 'root.crt', 
+                                         MockSigningServer.MOCK_ROOT_CERT )
+        self.signing_crt = self._writeFile( 'signing.crt', 
+                                            MockSigningServer.MOCK_SIGNING_CERT )
+        self.signing_key = self._writeFile( 'signing.key', 
+                                            MockSigningServer.MOCK_SIGNING_KEY )
+
+    def tearDown( self ):
+        shutil.rmtree( self.test_dir )
+
+    def _writeFile( self, filename, contents ):
+        path = os.path.join( self.test_dir, filename )
+        with open( path, 'w' ) as f:
+            f.write( contents )
+        return path
+
+    def _makeTestSwi( self, filename ):
+        path = os.path.join( self.test_dir, filename )
+        with zipfile.ZipFile( path, 'w' ) as swi:
+            swi.writestr( 'version', '4.21.0F' )
+        return path
+
+    def _validateNullSig( self, filename, size=8192, exists=True ):
+        with zipfile.ZipFile( filename, 'r', zipfile.ZIP_STORED ) as swi:
+            try:
+                sigFileInfo = swi.getinfo( SIG_FILE_NAME )
+            except KeyError:
+                self.assertFalse( exists )
+            else:
+                fileSize = sigFileInfo.file_size
+                self.assertEqual( fileSize, size )
+                self.assertTrue( exists )
+
+    def _generateHash( self, filename, blockSize=65536 ):
+        sha256sum = hashlib.sha256()
+        with open( filename, 'rb' ) as swiFile:
+            for block in iter( lambda: swiFile.read( blockSize ), b'' ):
+                sha256sum.update( block )
+        return sha256sum.hexdigest()
+
+    def _verifySignature( self, filename ):
+        retCode = verifyswi.verifySwi( filename, rootCA=self.root_crt )
+        self.assertEqual( retCode, 0 )
+
+    def test_prepare_return_hexdigest( self ):
+        hexdigest = swisignature.prepareSwi( self.test_swi )
+        actualHash = self._generateHash( self.test_swi ) 
+        self.assertEqual( hexdigest, actualHash )
+
+    def test_prepare_with_outfile( self ):
+        outfileName = 'EOS_null_sig.swi'
+        outfile = os.path.join( self.test_dir, outfileName )
+        swisignature.prepareSwi( self.test_swi, outfile )
+        self._validateNullSig( outfile )
+        self._validateNullSig( self.test_swi, exists=False )
+
+    def test_prepare_with_size( self ):
+        testSize = 9000
+        swisignature.prepareSwi( self.test_swi, size=testSize )
+        self._validateNullSig( self.test_swi, size=testSize )
+
+    def test_prepare_already_signed( self ):
+        with zipfile.ZipFile( self.test_swi, 'a' ) as swi:
+            swi.writestr( SIG_FILE_NAME, 'random signature' )
+        with self.assertRaises( swisignature.SwiSignException ):
+            swisignature.prepareSwi( self.test_swi )
+
+    def test_prepare_already_signed_force( self ):
+        with zipfile.ZipFile( self.test_swi, 'a' ) as swi:
+            swi.writestr( SIG_FILE_NAME, 'random signature' )
+        swisignature.prepareSwi( self.test_swi, forceSign=True )
+        self._validateNullSig( self.test_swi )
+
+    def test_sign_without_prepare( self ):
+        with self.assertRaises( swisignature.SwiSignException ):
+            swisignature.signSwi( self.test_swi, self.signing_crt, self.root_crt )
+
+    def test_sign_with_key( self ):
+        swisignature.prepareSwi( self.test_swi )
+        swisignature.signSwi( self.test_swi, self.signing_crt, self.root_crt,
+                              signingKeyFile=self.signing_key )
+        self._verifySignature( self.test_swi )
+
+    def test_sign_with_signature( self ):
+        swisignature.prepareSwi( self.test_swi )
+        signature = MockSigningServer.getTestSignature( self.test_swi )
+        sigFile = self._writeFile( 'signature.sig', signature )
+        swisignature.signSwi( self.test_swi, self.signing_crt, self.root_crt,
+                              signatureFile=sigFile )
+        self._verifySignature( self.test_swi )
+
+    def test_sign_with_bad_signature( self ):
+        swisignature.prepareSwi( self.test_swi )
+        not_base64_signature = 'a'
+        sigFile = self._writeFile( 'signature.sig', not_base64_signature )
+        with self.assertRaises( swisignature.SwiSignException ):
+            swisignature.signSwi( self.test_swi, self.signing_crt, self.root_crt,
+                                  signatureFile=sigFile )
+
+    def test_sign_and_verify_with_wrong_root_crt( self ):
+        swisignature.prepareSwi( self.test_swi )
+        with self.assertRaises( swisignature.SwiSignException ):
+            swisignature.signSwi( self.test_swi, self.signing_crt, self.signing_crt,
+                                  signingKeyFile=self.signing_key )
+
+    def test_sign_with_size_and_outfile( self ):
+        testSize = 9001
+        outfileName = 'EOS_null_sig.swi'
+        outfile = os.path.join( self.test_dir, outfileName )
+        swisignature.prepareSwi( self.test_swi, size=testSize, 
+                                 outfile=outfile )
+        swisignature.signSwi( outfile, self.signing_crt, self.root_crt,
+                              signingKeyFile=self.signing_key )
+        self._verifySignature( outfile )
+
+    def test_signature_bigger_than_null_sig( self ):
+        swisignature.prepareSwi( self.test_swi )
+        bigCertFile = self._writeFile( 'bigCrt.crt', 'a' * 9000 )
+        with self.assertRaises( swisignature.SwiSignException ):
+            swisignature.signSwi( self.test_swi, bigCertFile, self.root_crt,
+                                  signingKeyFile=self.signing_key )
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_verifyswix.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2018 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+
+import unittest
+import base64
+import os
+import shutil
+import tempfile
+import zipfile
+
+from switools import verifyswi
+from switools.verifyswi import VERIFY_SWI_RESULT
+
+import MockSigningServer
+
+SIG_FILE_NAME = 'swix-signature'
+
+# Random bad certificate
+BAD_SIGNING_CERT = """-----BEGIN CERTIFICATE-----
+MIICmzCCAYMCAQAwDQYJKoZIhvcNAQELBQAwDzENMAsGA1UEAwwEY2FwaTAgFw0x
+NDExMTMwMDM5MTFaGA8yMTE0MTAyMDAwMzkxMVowFjEUMBIGA1UEAwwLcGV0ZXJw
+YW4gMTMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCwqhgAs7sgPEgv
+jCQDCktqTjqfSBtlfffD3+LELy5dnu3YwU/CRPSzeLAnLLQB8K4wPgCkkrJcJdQV
+sCRJ7n5MYibOGA5Up9A0jCFdszlOZWFswCwPrkeg8onj61KT8qcPonw4hpdI2GDr
+vFg+lxTN6u+k7R0K63j/7X5aiO1bkmnV9wOOKJOmfulpgiFjWBKi/0zlD4PlQ57O
+HivJ+jmVKMZzNfe2jeWuKqGB8k2HJtvg3oleu5vt8m1XcyMqxMCle9J8JYHcfSTs
+86q2r+eexkzPoMvAwmaUhQ+JDsaGJE+ZCq4S7JEctyAwBaOqRE3J6n46EcZ/Kfxz
+OYAZBKRXAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAJxn5eDR2QUcbJtCQ3U4VcpE
+VgMOcLDzWIazmTvgR3ga3JNJqsHwgSfpZ5oTVAaIn+TFfxLmVT1Mi6XPNz1HdgWM
+91YiSiY6PLoAYoUKShZpPqbMIeQUNKC764MH2fC8VpvJlvT9f6Hhq0mwC/GXbiAd
+YJhdChI0J5eyqCzT/2Ahmudpk4S25ehCcOEugZRFR46JETSu/yO5XzJmFqa2kFVf
+i2rEL8Vw0Qmt9qlsoUwE5AA5toEP2dguBLz2PIb6/uYvMW0DoLqVIyeNKT/C+6ND
+mNkSQf6ILPqzfAPhwg7/s9Y7X4UqM1ex0yiryw2iX5LqVkS9rLIBtNsmHoH1Xpw=
+-----END CERTIFICATE-----"""
+
+FAKE_SIGNATURE = "MEUCIQCK5BtHiuFn+hoV8GnIICc60AUi1rgKtOtrguIUz4g5/gIgHXT5beCabs\
+        hhWhuItZFGEop77TPhaA9ha2eh4FKRd+8="
+
+class TestVerifyBadSignature( unittest.TestCase ):
+    def setUp( self ):
+        self.test_dir = tempfile.mkdtemp()
+        self.root_crt = self._writeFile( 'root.crt', 
+                                         MockSigningServer.MOCK_ROOT_CERT )
+        self.test_swix = os.path.join( self.test_dir, 'Test.swix' )
+        with zipfile.ZipFile( self.test_swix, 'w' ) as swix:
+            swix.writestr( 'manifest', 'list of rpms' )
+
+    def tearDown( self ):
+        shutil.rmtree( self.test_dir )
+
+    def _writeFile( self, filename, contents ):
+        path = os.path.join( self.test_dir, filename )
+        with open( path, 'w' ) as f:
+            f.write( contents )
+        return path
+
+    def _addSigToSwix( self, signature ):
+        with zipfile.ZipFile( self.test_swix, 'a' ) as swix:
+            swix.writestr( SIG_FILE_NAME, signature )
+
+    def _makeSwixSignature( self, signingCert=MockSigningServer.MOCK_SIGNING_CERT,
+                           signature=FAKE_SIGNATURE,
+                           hashAlgo='SHA-256' ):
+        swixSignature = {}
+        swixSignature[ 'IssuerCert' ] = base64.standard_b64encode( signingCert )
+        swixSignature[ 'HashAlgorithm' ] = hashAlgo
+        swixSignature[ 'Signature' ] = signature
+
+        swixSigStr = ''
+        for key, value in swixSignature.iteritems():
+            swixSigStr += "%s:%s\n" % ( key, value )
+        return swixSigStr
+
+    def test_no_signature( self ):
+        retCode = verifyswi.verifySwi( self.test_swix )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_SIGNATURE_FILE )
+
+    def test_not_a_zip_file( self ):
+        testFile = self._writeFile( 'notaswix', 'stuff' )
+        retCode = verifyswi.verifySwi( testFile )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_NOT_A_SWI )
+
+    def test_untrusted_signing_cert( self ):
+        sig = self._makeSwixSignature( signingCert=BAD_SIGNING_CERT )
+        self._addSigToSwix( sig )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=self.root_crt )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_CERT_MISMATCH )
+
+    def test_invalid_sig( self ):
+        sig = self._makeSwixSignature()
+        self._addSigToSwix( sig )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=self.root_crt )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_VERIFICATION )
+
+    def test_invalid_hash_algo( self ):
+        sig = self._makeSwixSignature( hashAlgo='SHA-512' )
+        self._addSigToSwix( sig )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=self.root_crt )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_HASH_ALGORITHM )
+
+    def test_malformed_signature( self ):
+        self._addSigToSwix( 'bad sig' )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=self.root_crt )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_SIGNATURE_FORMAT )
+
+    def test_malformed_signature_key_value( self ):
+        self._addSigToSwix( 'a:b\nc:d' )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=self.root_crt )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_SIGNATURE_FORMAT )
+
+    def test_malformed_signing_crt( self ):
+        sig = self._makeSwixSignature( signingCert='bad cert' )
+        self._addSigToSwix( sig )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=self.root_crt )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_INVALID_SIGNING_CERT )
+
+    def test_malformed_root_crt( self ):
+        sig = self._makeSwixSignature()
+        self._addSigToSwix( sig )
+        rootCa = self._writeFile( 'root.crt', 'bad cert' )
+        retCode = verifyswi.verifySwi( self.test_swix, rootCA=rootCa )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_INVALID_ROOT_CERT )
+
+    def test_use_arista_default_root_ca( self ):
+        sig = self._makeSwixSignature()
+        self._addSigToSwix( sig )
+        retCode = verifyswi.verifySwi( self.test_swix )
+        self.assertEqual( retCode, VERIFY_SWI_RESULT.ERROR_CERT_MISMATCH ) 
+
+if __name__ == '__main__':
+    unittest.main()
